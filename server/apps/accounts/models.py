4488cb65ccfb787cfa2d59c1690ff8d8f8b4877f
Phoebe M <pxm474@student.bham.ac.uk>
2026-03-11 16:45:24 +0000

    Updated habitability dataset and database models

--- server/apps/accounts/models.py
+++ server/apps/accounts/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 
+class Region(models.Model):
+    region_id = models.AutoField(primary_key=True)
+    region_name = models.CharField(max_length=20)
+    # Calculate habitability score from values when needed 
+
+    # For our use/ testing
+    def __str__(self):
+        return self.region_name
 
 
+class Neighbourhood(models.Model):
+    neighbourhood_id = models.AutoField(primary_key=True)
+    region = models.ForeignKey("Region", on_delete=models.CASCADE, related_name="Neighbourhoods")
+    county = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=7)
+
+    # For our use/ testing
+    def __str__(self):
+        return self.postcode
 class HabitabilityScores(models.Model):
 
-    #score_id = models.AutoField(primary_key=True)
+    score_id = models.AutoField(primary_key=True)
 
     #neighbourhood = models.ForeignKey("Neighbourhood", on_delete=models.CASCADE)
 
     county = models.CharField(max_length=100, unique=True)
+    
     employment_rate = models.FloatField()
     income = models.FloatField()
     inflation = models.FloatField()
@@ -24,7 +42,7 @@ class HabitabilityScores(models.Model):
     disaster_risks = models.FloatField()
     civic_engagement = models.FloatField()
 
-    #date_scored = models.DateTimeField()
+    date_scored = models.DateField()
 
     # For our use/ testing
     def __str__(self):
