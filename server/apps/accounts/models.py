f407c7092cfa7a01daeec003e1c97f4d7056dc66
Phoebe M <pxm474@student.bham.ac.uk>
2026-03-11 15:24:31 +0000

    Imported the county csv dataset for the habitabilityScores model

--- server/apps/accounts/models.py
+++ server/apps/accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+
+class HabitabilityScores(models.Model):
+
+    #score_id = models.AutoField(primary_key=True)
+
+    #neighbourhood = models.ForeignKey("Neighbourhood", on_delete=models.CASCADE)
+
+    county = models.CharField(max_length=100, unique=True)
+    employment_rate = models.FloatField()
+    income = models.FloatField()
+    inflation = models.FloatField()
+    air_quality = models.FloatField()
+    water_quality = models.FloatField()
+    land_fertility = models.FloatField()
+    waste_management = models.FloatField()
+    education = models.FloatField()
+    healthcare = models.FloatField()
+    transportation = models.FloatField()
+    crime = models.FloatField()
+    politics = models.FloatField()
+    disaster_risks = models.FloatField()
+    civic_engagement = models.FloatField()
+
+    #date_scored = models.DateTimeField()
+
+    # For our use/ testing
+    def __str__(self):
+        return self.county
