35422ae05609782c384c7d1d117004a826fad776
Phoebe M <pxm474@student.bham.ac.uk>
2026-03-11 17:13:13 +0000

    Added comments class to Reports, but commented out as it depends on Report and User models

--- server/apps/reports/models.py
+++ server/apps/reports/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+'''
+class Comments(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    report = models.ForeignKey("reports.Report", on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE, related_name="comments")
+    reply_to = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="replies")
+    description = models.TextField()
+
+    # For our use/ testing
+    def __str__(self):
+        return f"Comment(self.commentID)"
+        '''
