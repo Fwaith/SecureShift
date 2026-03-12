a307f0d3cf0b99174a88cd1c37ffcaa325103134
chloe <cjc403@student.bham.ac.uk>
2026-03-12 21:23:58 +0000

    map view added, focusing on UK. birmingham as midpoint

--- client/src/views/MapView.vue
+++ client/src/views/MapView.vue
@@ -1,6 +1,35 @@
 <template>
-  <div>
-    <h1>Map View</h1>
-    <p>Map will be displayed here</p>
-  </div>
+    <div class="map-page">
+        <l-map
+            v-model:zoom="zoom"
+            :center="center"
+            class="leaflet-map"
+        >
+        <l-tile-layer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; OpenStreetMap contributors"
+        />
+        </l-map>
+    </div>
 </template>
+
+<script setup>
+import { ref } from "vue"
+import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet"
+import "leaflet/dist/leaflet.css"
+
+const zoom = ref(7)
+const center = ref([52.48, -1.89])
+</script>
+
+<style scoped>
+.map-page {
+    width: 100%;
+    height: 100vh;
+}
+
+.leaflet-map {
+    width: 100%;
+    height: 100%;
+}
+</style>
