f7d5469fe5a25e6357539e66754971ea1b426617
unknown <yxd419@student.bham.ac.uk>
2026-03-14 01:07:20 +0000

    Updated server/apps/accounts/models.py

--- server/apps/accounts/models.py
+++ server/apps/accounts/models.py
@@ -1,2 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=20, blank=True, default='')
+    postcode = models.CharField(max_length=10, blank=True, default='')
+
+    def __str__(self):
+        return self.username
