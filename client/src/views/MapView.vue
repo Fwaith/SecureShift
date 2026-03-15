4411518f0f637b6e86eb38b1c858cd33b1070f55
chloe <cjc403@student.bham.ac.uk>
2026-03-15 17:27:46 +0000

    when clicked on marker, zoom in on flood area

--- client/src/views/MapView.vue
+++ client/src/views/MapView.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="map-page">
-        <l-map v-model:zoom="zoom" :center="center" class="leaflet-map">
-        <l-tile-layer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution="&copy; OpenStreetMap contributors"
-        />
+        <l-map ref="mapRef" v-model:zoom="zoom" :center="center" class="leaflet-map">
+            <l-tile-layer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; OpenStreetMap contributors"
+            />
 
-        <l-marker :lat-lng="[52.48, -1.89]">
-            <l-popup>
-                Flood reported in Birmingham
-            </l-popup>
-        </l-marker>
+            <l-marker 
+                :lat-lng="floodLocation"
+                @click="zoomToFlood"
+            >
+                <l-popup>
+                    Flood reported
+                </l-popup>
+            </l-marker>
         </l-map>
     </div>
 </template>
@@ -20,8 +23,14 @@ import { ref } from "vue"
 import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet"
 import "leaflet/dist/leaflet.css"
 
-const zoom = ref(7)
-const center = ref([52.48, -1.89])
+const zoom = ref(6)
+const center = ref([52.3555, -1.1743]) //uk
+const floodLocation = [52.48, -1.89] //birm
+const mapRef = ref(null)
+const zoomToFlood = () => {
+    const map = mapRef.value.leafletObject
+    map.flyTo(floodLocation, 13)
+}
 </script>
 
 <style scoped>
