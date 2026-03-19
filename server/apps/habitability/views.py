b409402a9d3934ecb123193341e6cb1fb262e07e
Charles Homer <cah464@student.bham.ac.uk>
2026-03-19 22:46:17 +0000

    Add /neighbourhoods

--- server/apps/habitability/views.py
+++ server/apps/habitability/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+from .models import Neighborhood
+
+@csrf_exempt
+@require_http_methods(["GET"])
+def get_neighbourhoods(request):
+	neighbourhoods = Neighborhood.objects.all().order_by("neighborhood_id")
+	payload = [
+		{
+			"neighbourhoodId": neighbourhood.neighborhood_id,
+			"postcode": neighbourhood.postcode or "",
+		}
+		for neighbourhood in neighbourhoods
+	]
+	return JsonResponse(payload, safe=False)
